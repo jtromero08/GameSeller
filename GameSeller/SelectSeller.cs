b6e0525eb5a104680ec659e1e4135f32fb2c328b
jtromero08 <jtromero08@gmail.com>
2019-10-22 21:53:53 -0400

    Added 2 Method: SelectedSeller & SelectingSeller.

--- GameSeller/SelectSeller.cs
+++ GameSeller/SelectSeller.cs
@@ -6,15 +6,40 @@ namespace GameSeller
 {
     public class SelectSeller
     {
+
+        public void SelectedSeller()
+        {
+            string seller = Console.ReadLine();
+
+            // Here I check if the number that was enter
+            string message = Int32.TryParse(seller, out int n) ? 
+                $"You have selected Seller_{seller}" :
+                $"Error: '{seller}' is not a number";
+
+            while(message == $"Error: '{seller}' is not a number")
+            {
+                Console.WriteLine("Remember only number\n" +
+                    "Please select your seller again: \n");
+                seller = Console.ReadLine();
+
+                message = Int32.TryParse(seller, out n) ?
+                $"You have selected Seller_{seller}" :
+                $"Error: '{seller}' is not a number";
+                
+            }
+            Console.WriteLine($"{message}\n");
+        }
         public void SelectingSeller(string name)
         {
             Console.WriteLine($"Welcome {name} to GameSellerApp!");
-            Console.WriteLine("Please select your sellers: \n");
+            Console.WriteLine("Please select your seller by entering the number: \n");
 
             for(var i = 0; i < 4; i++)
             {
                 Console.WriteLine($"Seller_{i + 1}");
             }
+            Console.WriteLine("\n");
+            SelectedSeller();
         }
     }
 }
